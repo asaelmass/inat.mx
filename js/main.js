618ee4192afbc5d1aa66a36dfe9a346be8f25800
unknown <amassl@hotmail.com>
2022-03-14 17:54:22 -0600

    140322

--- js/main.js
+++ js/main.js
@@ -2,6 +2,25 @@ $(document).ready(function() {
     $(".menu-icon").on("click", function() {
           $("nav ul").toggleClass("showing");
     });
+
+
+    $.getJSON("http://localhost/inat/inat.mx/trofologos.json", function(data){
+          $.each(data[0].paises, function(v,k){
+            console.log("Pais:"+k.nombre);
+            $.each(this.ciudades, function(v,k){
+                  console.log("ciudad:"+k);
+                });
+          });
+          $.each(data[0].items, function(v,k){
+                console.log("Trofologo: "+k.nombre +" Ciudad: "+ k.ciudad)
+
+          });
+          
+      }).fail(function(){
+            console.log("An error has occurred.");
+      });
+
+
 });
 
 // Scrolling Effect
